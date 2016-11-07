75f919bf3d62b13ff8fa02d0bd032a9da32a73ad
NotTheEvilOne <git-nteo@vplace.de>
2016-11-07 08:54:26 +0100

    Trivial: Fixed PEP 8 inconsistent indentation

--- unittest/test_rfc_email_message.py
+++ unittest/test_rfc_email_message.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-##j## BOF
 
 """
 RFC e-mail for Python
@@ -18,30 +17,26 @@ https://www.direct-netware.de/redirect?licenses;mpl2
 #echo(__FILEPATH__)#
 """
 
-from os import path
 import re
-import sys
 import unittest
 
 from dNG.data.rfc.email.message import Message
 from dNG.data.rfc.email.part import Part
 
 class TestRfcEMailPart(unittest.TestCase):
-#
-	def test_plain(self):
-	#
-		"""
+    def test_plain(self):
+        """
 Test basic methods for an ASCII attachment.
-		"""
+        """
 
-		part = Part(Part.TYPE_MESSAGE_BODY, "text/plain", "Hello world")
+        part = Part(Part.TYPE_MESSAGE_BODY, "text/plain", "Hello world")
 
-		message = Message()
-		message.set_subject("Test message")
-		message.add_body(part)
+        message = Message()
+        message.set_subject("Test message")
+        message.add_body(part)
 
-		self.assertEqual(
-			"""MIME-Version: 1.0
+        self.assertEqual(
+            """MIME-Version: 1.0
 Content-Transfer-Encoding: quoted-printable
 Content-Type: text/plain; charset="UTF-8"
 To: undisclosed-recipients
@@ -49,27 +44,26 @@ Date: Thu, 01 Jan 1970 00:00:00 GMT
 Subject: Test message
 
 Hello world""",
-			TestRfcEMailPart._get_unified_message_as_string(message)
-		)
-	#
+            TestRfcEMailPart._get_unified_message_as_string(message)
+                        )
+    #
 
-	def test_multipart_alternative(self):
-	#
-		"""
+    def test_multipart_alternative(self):
+        """
 Test basic methods for an ASCII attachment.
-		"""
+        """
 
-		part = Part(Part.TYPE_MESSAGE_BODY, "text/plain", "Hello world")
+        part = Part(Part.TYPE_MESSAGE_BODY, "text/plain", "Hello world")
 
-		message = Message()
-		message.set_subject("Test message")
-		message.add_body(part)
+        message = Message()
+        message.set_subject("Test message")
+        message.add_body(part)
 
-		part = Part(Part.TYPE_MESSAGE_BODY, "application/xhtml+xml", "<p>This is broken by design</p>")
-		message.add_body(part)
+        part = Part(Part.TYPE_MESSAGE_BODY, "application/xhtml+xml", "<p>This is broken by design</p>")
+        message.add_body(part)
 
-		self.assertEqual(
-			"""MIME-Version: 1.0
+        self.assertEqual(
+            """MIME-Version: 1.0
 Content-Type: multipart/alternative; boundary="===============x=="
 To: undisclosed-recipients
 Date: Thu, 01 Jan 1970 00:00:00 GMT
@@ -88,34 +82,33 @@ Content-Type: application/xhtml+xml; charset="UTF-8"
 
 <p>This is broken by design</p>
 --===============x==--""",
-			TestRfcEMailPart._get_unified_message_as_string(message)
-		)
-	#
+            TestRfcEMailPart._get_unified_message_as_string(message)
+                        )
+    #
 
-	def test_multipart_mixed(self):
-	#
-		"""
+    def test_multipart_mixed(self):
+        """
 Test basic methods for an ASCII attachment.
-		"""
+        """
 
-		part = Part(Part.TYPE_MESSAGE_BODY, "text/plain", "Hello world")
+        part = Part(Part.TYPE_MESSAGE_BODY, "text/plain", "Hello world")
 
-		message = Message()
-		message.set_subject("We like German Umlauts to test UTF-8 öäü")
-		message.add_body(part)
+        message = Message()
+        message.set_subject("We like German Umlauts to test UTF-8 öäü")
+        message.add_body(part)
 
-		part = Part(Part.TYPE_INLINE, "text/plain", "Hello world", file_name = "test.txt")
-		message.add_body_related_attachment(part)
+        part = Part(Part.TYPE_INLINE, "text/plain", "Hello world", file_name = "test.txt")
+        message.add_body_related_attachment(part)
 
-		part = Part(Part.TYPE_ATTACHMENT, "text/plain", "Hello world", file_name = "test.txt")
-		message.add_attachment(part)
+        part = Part(Part.TYPE_ATTACHMENT, "text/plain", "Hello world", file_name = "test.txt")
+        message.add_attachment(part)
 
-		part = Part(Part.TYPE_ATTACHMENT, "text/plain", "Hello world 2", file_name = "test2.txt")
-		message.add_attachment(part)
-		self.maxDiff = None
+        part = Part(Part.TYPE_ATTACHMENT, "text/plain", "Hello world 2", file_name = "test2.txt")
+        message.add_attachment(part)
+        self.maxDiff = None
 
-		self.assertEqual(
-			"""MIME-Version: 1.0
+        self.assertEqual(
+            """MIME-Version: 1.0
 Content-Type: multipart/mixed; boundary="===============x=="
 To: undisclosed-recipients
 Date: Thu, 01 Jan 1970 00:00:00 GMT
@@ -157,34 +150,29 @@ Content-Disposition: attachment; filename="test2.txt"
 
 Hello world 2
 --===============x==--""",
-			TestRfcEMailPart._get_unified_message_as_string(message)
-		)
-	#
+            TestRfcEMailPart._get_unified_message_as_string(message)
+                        )
+    #
 
-	@staticmethod
-	def _get_unified_message_as_string(message):
-	#
-		"""
+    @staticmethod
+    def _get_unified_message_as_string(message):
+        """
 Returns the message in a unified (but not standard conform) way.
 
 :return: (str) Unified formatted message
-		"""
+        """
 
-		_return = re.sub("===============\\d+==", "===============x==", message.as_string())
-		_return = re.sub("\n boundary=\"===============x==\"\n", " boundary=\"===============x==\"\n", _return)
-		_return = re.sub("^Content-ID: <cid\\d+@mail>$", "Content-ID: <cidx@mail>", _return, flags = re.M)
-		_return = re.sub("^Date: \\w{3}, \\d{2} \\w{3} \\d{4} \\d{2}:\\d{2}:\\d{2} GMT$", "Date: Thu, 01 Jan 1970 00:00:00 GMT", _return, flags = re.M)
+        _return = re.sub("===============\\d+==", "===============x==", message.as_string())
+        _return = re.sub("\n boundary=\"===============x==\"\n", " boundary=\"===============x==\"\n", _return)
+        _return = re.sub("^Content-ID: <cid\\d+@mail>$", "Content-ID: <cidx@mail>", _return, flags = re.M)
+        _return = re.sub("^Date: \\w{3}, \\d{2} \\w{3} \\d{4} \\d{2}:\\d{2}:\\d{2} GMT$", "Date: Thu, 01 Jan 1970 00:00:00 GMT", _return, flags = re.M)
 
-		_return = _return.replace("--===============x==--\n\n", "--===============x==--\n")
+        _return = _return.replace("--===============x==--\n\n", "--===============x==--\n")
 
-		return _return.strip()
-	#
+        return _return.strip()
+    #
 #
 
 if (__name__ == "__main__"):
+    unittest.main()
 #
-	sys.path.append(path.normpath("../src"))
-	unittest.main()
-#
-
-##j## EOF
--- unittest/test_rfc_email_part.py
+++ unittest/test_rfc_email_part.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-##j## BOF
 
 """
 RFC e-mail for Python
@@ -18,58 +17,49 @@ https://www.direct-netware.de/redirect?licenses;mpl2
 #echo(__FILEPATH__)#
 """
 
-from os import path
-import sys
 import unittest
 
 from dNG.data.rfc.email.part import Part
 
 class TestRfcEMailPart(unittest.TestCase):
-#
-	def test_ascii_attachment(self):
-	#
-		"""
+    def test_ascii_attachment(self):
+        """
 Test basic methods for an ASCII attachment.
-		"""
+        """
 
-		part = Part(Part.TYPE_ATTACHMENT, "text/plain", "Hello world", file_name = "hello_world.txt")
-		self.assertEqual(Part.TYPE_ATTACHMENT, part.get_part_type())
+        part = Part(Part.TYPE_ATTACHMENT, "text/plain", "Hello world", file_name = "hello_world.txt")
+        self.assertEqual(Part.TYPE_ATTACHMENT, part.get_part_type())
 
-		_exception = None
+        _exception = None
 
-		try: Part(Part.TYPE_ATTACHMENT, "text/plain", "Hello world")
-		except TypeError as handled_exception: _exception = handled_exception
+        try: Part(Part.TYPE_ATTACHMENT, "text/plain", "Hello world")
+        except TypeError as handled_exception: _exception = handled_exception
 
-		self.assertTrue(isinstance(_exception, TypeError))
+        self.assertTrue(isinstance(_exception, TypeError))
 
-		# This test uses German special characters
+        # This test uses German special characters
 
-		part = Part(Part.TYPE_ATTACHMENT, "text/plain", "Hallo Welt, schön das du dich drehst.", file_name = "hallo_welt_öäü.txt")
-		self.assertEqual("Hallo Welt, sch=C3=B6n das du dich drehst.", part.get_payload())
-	#
+        part = Part(Part.TYPE_ATTACHMENT, "text/plain", "Hallo Welt, schön das du dich drehst.", file_name = "hallo_welt_öäü.txt")
+        self.assertEqual("Hallo Welt, sch=C3=B6n das du dich drehst.", part.get_payload())
+    #
 
-	def test_ascii_inline(self):
-	#
-		"""
+    def test_ascii_inline(self):
+        """
 Test basic methods for an ASCII inline part.
-		"""
+        """
 
-		part = Part(Part.TYPE_INLINE, "text/plain", "Hello world", file_name = "hello_world.txt")
-		self.assertEqual(Part.TYPE_INLINE, part.get_part_type())
+        part = Part(Part.TYPE_INLINE, "text/plain", "Hello world", file_name = "hello_world.txt")
+        self.assertEqual(Part.TYPE_INLINE, part.get_part_type())
 
-		_exception = None
+        _exception = None
 
-		try: Part(Part.TYPE_ATTACHMENT, "text/plain", "Hello world")
-		except TypeError as handled_exception: _exception = handled_exception
+        try: Part(Part.TYPE_ATTACHMENT, "text/plain", "Hello world")
+        except TypeError as handled_exception: _exception = handled_exception
 
-		self.assertTrue(isinstance(_exception, TypeError))
-	#
+        self.assertTrue(isinstance(_exception, TypeError))
+    #
 #
 
 if (__name__ == "__main__"):
+    unittest.main()
 #
-	sys.path.append(path.normpath("../src"))
-	unittest.main()
-#
-
-##j## EOF
